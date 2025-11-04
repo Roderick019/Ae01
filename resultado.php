9af1fe7f7b1ad80efde03e840cf7621ca21f3cc7
RODRIGO ROLDAN CANYADAS <roroca02@alumnatflorida.es>
2025-11-04 11:48:04 +0100

    cambio de la act 2 realizados

---
+++ resultado.php
@@ -0,0 +1,12 @@
+<?php echo "Aquí tienes el resultado !"; ?>
+<!DOCTYPE html> 
+<html> 
+<head> 
+    <title>resultado</title> 
+    <link rel="stylesheet" type="text/css" href="styles.css"> 
+</head> 
+<body>
+<button onclick="window.location.href='index.php'">Jugar de nuevo</button>
+
+</body>
+ </html>
